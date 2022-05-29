9c8a8e16adf203a1d125a5cfff99d29627005408
alexbelz123 <belz.alexx@gmail.com>
2022-05-29 21:15:38 +0300

    added react advanced patterns

--- hooks/useAsync.jsx
+++ hooks/useAsync.jsx
@@ -1,32 +1,82 @@
-const useAsync = (asyncFunction, immediate = true) => {
-  const [status, setStatus] = useState('idle');
-  const [value, setValue] = useState(null);
-  const [error, setError] = useState(null);
-  // The execute function wraps asyncFunction and
-  // handles setting state for pending, value, and error.
-  // useCallback ensures the below useEffect is not called
-  // on every render, but only if asyncFunction changes.
-  const execute = useCallback(() => {
-    setStatus('pending');
-    setValue(null);
-    setError(null);
-    return asyncFunction()
-      .then((response) => {
-        setValue(response);
-        setStatus('success');
-      })
-      .catch((error) => {
-        setError(error);
-        setStatus('error');
-      });
-  }, [asyncFunction]);
-  // Call execute if we want to fire it right away.
-  // Otherwise execute can be called later, such as
-  // in an onClick handler.
-  useEffect(() => {
-    if (immediate) {
-      execute();
+import * as React from 'react';
+
+function useSafeDispatch(dispatch) {
+  const mounted = React.useRef(false);
+
+  React.useLayoutEffect(() => {
+    mounted.current = true;
+    return () => {
+      mounted.current = false;
+    };
+  }, []);
+
+  return React.useCallback(
+    (...args) => (mounted.current ? dispatch(...args) : void 0),
+    [dispatch]
+  );
+}
+
+function asyncReducer(state, action) {
+  switch (action.type) {
+    case 'pending': {
+      return { status: 'pending', data: null, error: null };
     }
-  }, [execute, immediate]);
-  return { execute, status, value, error };
-};
+    case 'resolved': {
+      return { status: 'resolved', data: action.data, error: null };
+    }
+    case 'rejected': {
+      return { status: 'rejected', data: null, error: action.error };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
+
+function useAsync(initialState) {
+  const [state, unsafeDispatch] = React.useReducer(asyncReducer, {
+    status: 'idle',
+    data: null,
+    error: null,
+    ...initialState,
+  });
+
+  const dispatch = useSafeDispatch(unsafeDispatch);
+
+  const { data, error, status } = state;
+
+  const run = React.useCallback(
+    (promise) => {
+      dispatch({ type: 'pending' });
+      promise.then(
+        (data) => {
+          dispatch({ type: 'resolved', data });
+        },
+        (error) => {
+          dispatch({ type: 'rejected', error });
+        }
+      );
+    },
+    [dispatch]
+  );
+
+  const setData = React.useCallback(
+    (data) => dispatch({ type: 'resolved', data }),
+    [dispatch]
+  );
+  const setError = React.useCallback(
+    (error) => dispatch({ type: 'rejected', error }),
+    [dispatch]
+  );
+
+  return {
+    setData,
+    setError,
+    error,
+    status,
+    data,
+    run,
+  };
+}
+
+export { useAsync };
