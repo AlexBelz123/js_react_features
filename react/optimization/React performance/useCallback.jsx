ae4b709a5abc4d7a0493b666003046f316332437
alexbelz123 <belz.alexx@gmail.com>
2022-07-04 13:06:59 +0300

    typo fix

--- react/optimization/React performance/useCallback.jsx
+++ react/optimization/React performance/useCallback.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-// lets say Child component have a lot of rerenders and cuse bad UX for some reason
+// lets say Child component have a lot of rerenders and cause bad UX for some reason
 // so we want to prevent this bad UX and make less rerenders, right? How we can achieve it?
-// of cause we can rewrite our code logic etc, but in our case we will use React.memo with react.useCallback
+// of cause we can rewrite our code logics, but in our case we will use React.memo with react.useCallback
 
 const App = () => {
   const [formData, setFormData] = React.useState();
@@ -31,11 +31,12 @@ let Child = ({ formData, handleSubmit }) => {
     </form>
   );
 };
-// if props are the same - the component will not be rerendered
+// if props are the same - the component will not be rerendered (React.memo)
 // so why we are using useCallback on line 10 ? Because handleSubmit is a function, whicn means
 // that on each render it will create new reference for this function, so every time our
 // <Child /> component will take new reference (new prop) and make a rerender. UseCallback memoizes
-// this reference and prevent child component from unnecessary rerender (the reference can be changed if we will provide some dependencies to empty array (line 13))
+// this reference and prevent child component from unnecessary rerenders
+// (the reference can be changed if we will provide some dependencies to empty array (line 13))
 Child = React.memo(Child);
 
 export default App;
